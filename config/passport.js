8ae8c1db8f4d8c7ab3bd3256a4f4fd8d130d46f3
Jordan Chadsey <jordan@identipop.com>
2017-12-16 21:55:03 -0800

    login signup

--- config/passport.js
+++ config/passport.js
@@ -10,6 +10,102 @@ module.exports = function(passport, user) {
     var LocalStrategy = require('passport-local').Strategy;
 
 
+    //serialize
+    passport.serializeUser(function(user, done) {
+
+        done(null, user.id);
+
+    });
+
+    // deserialize user
+    passport.deserializeUser(function(id, done) {
+
+        User.findById(id).then(function(user) {
+
+            if (user) {
+
+                done(null, user.get());
+
+            } else {
+
+                done(user.errors, null);
+
+            }
+
+        });
+
+    });
+
+    passport.use('local-signin', new LocalStrategy(
+
+        {
+
+            // by default, local strategy uses username and password, we will override with email
+
+            usernameField: 'username',
+
+            passwordField: 'password',
+
+            passReqToCallback: true // allows us to pass back the entire request to the callback
+
+        },
+
+
+        function(req, email, password, done) {
+
+            var User = user;
+
+            var isValidPassword = function(userpass, password) {
+
+                return bCrypt.compareSync(password, userpass);
+
+            };
+
+            User.findOne({
+                where: {
+                    username: username
+                }
+            }).then(function(user) {
+
+                if (!user) {
+
+                    return done(null, false, {
+                        message: 'Username does not exist'
+                    });
+
+                }
+
+                if (!isValidPassword(user.password, password)) {
+
+                    return done(null, false, {
+                        message: 'Incorrect password.'
+                    });
+
+                }
+
+
+                var userinfo = user.get();
+                return done(null, userinfo);
+
+
+            }).catch(function(err) {
+
+                console.log("Error:", err);
+
+                return done(null, false, {
+                    message: 'Something went wrong with your sign-in.'
+                });
+
+            });
+
+
+        }
+
+    ));
+
+
+
+
     passport.use('local-signup', new LocalStrategy(
 
         {
@@ -88,7 +184,7 @@ module.exports = function(passport, user) {
             });
 
         }
- 
+
     ));
 
 };
