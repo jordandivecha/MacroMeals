cc9817e0d7bdb8b29fecb10f361e8114e71afda1
Jordan Chadsey <jordan@identipop.com>
2017-12-18 11:39:58 -0800

    pull back data

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,11 +1,12 @@
 // sends the user to the index page
+var db = require("../models");
 module.exports = function (app){
 // app.get("/", function(req, res) {
 //
 //   res.render("index");
 // });
 
-var db = require("../models");
+
 // sends user to login page
 app.get("/dashboard", function(req, res){
 res.render("dashboard");
@@ -40,7 +41,7 @@ app.get("/mealsearch", function (req,res){
   var id = req.session.passport.user;
   console.log(id);
   db.user.findAll(
-    {where: {"id": id}}
+    {where: {id: id}}
 
   ).then(function(err, result) {
     console.log(result);
@@ -49,7 +50,13 @@ app.get("/mealsearch", function (req,res){
 });
 
 app.get("/mealbox", function (req,res){
-  res.render("mealbox");
+  var id = req.session.passport.user;
+  console.log(id);
+  db.Meal.findAll({}).then(function(result){
+    console.log(JSON.stringify(result));
+    res.render("mealbox", {mealbox: result});
+  });
+
 });
 
 app.get("/ingredientsearch", function (req,res){
